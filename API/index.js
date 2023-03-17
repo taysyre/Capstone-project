9b350270b79e1fb8c3a415c8c2170bf8e8ccb0ad
taysyre <tayapril3@gmail.com>
2023-03-17 09:34:58 +0200

    Update

--- API/index.js
+++ API/index.js
@@ -16,12 +16,14 @@ express.json: setting the content-type to application/json
 bodyParser.urlencoded( {extended: true} ): Object will contain
 values of any type instead of just a string
 */
+const bcrypt = require('bcryptjs') 
+
 app.use((req, res, next)=> {
         res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
         res.header("Access-Control-Allow-Credentials", "true")
         res.header("Access-Control-Allow-Methods", "*")
         res.header("Access-Control-Allow-Headers", "*")
-        next();
+        next();  
 });
 app.use(route);
 app.use(
