79eed5ed74c6e1d06dc0074e6c350c3c87574e29
taysyre <tayapril3@gmail.com>
2023-03-23 15:36:27 +0200

    update

--- API/index.js
+++ API/index.js
@@ -1,36 +1,37 @@
-const path = require('path');
 const express = require('express');
+// route
+const route = require('./controller');
+// cors
 const cors = require('cors');
+// port 
+const port = parseInt(process.env.PORT) || 9001;
+// Express app
 const app = express();
-const {userRoutes, orderRoutes, productRoutes} = require('./routes') 
-require('dotenv').config
-const port = process.env.PORT || 6969;
+// Middleware
+const {errorHandling} = require('./middleware/ErrorHandling');
+//
+const cookieParser = require('cookie-parser');
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.json(), cors());
-
-// Allowing frontend access to backend
 app.use((req, res, next)=> {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8082')
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Credentials", "true")
-  res.header("Access-Control-Allow-Methods", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next();
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header("Access-Control-Allow-Credentials", "true")
+        res.header("Access-Control-Allow-Methods", "*")
+        res.header("Access-Control-Allow-Headers", "*")
+        next();
 });
 
-// Root Route
-app.get('/', (req, res) => {
-  res.status(200);
-  res.sendFile(path.join(__dirname, '/view/index.html'));
-})
-// Use router to handle product and user routes
-app.use('/users', userRoutes);
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes)
+app.use(route);
+app.use(
+    cors(),
+    cookieParser(),
+    express.json,
+    express.urlencoded({extended: false})
+)
 
-
-app.listen(port, () => {
-  console.log('Server is running...');
-  console.log(`Listening on port ${port}...`);
-})
+// Server is running
+app.listen(port, ()=> {
+    console.log(`Server is running...`);
+    console.log(`Listening on port ${port}...`);
+});
+// Handling all errors
+app.use(errorHandling);
