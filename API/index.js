7b340a0e2059d52931b7a269566027eed14c0a36
taysyre <tayapril3@gmail.com>
2023-03-25 10:44:52 +0200

    update

--- API/index.js
+++ API/index.js
@@ -1,43 +1,35 @@
+const path = require('path');
 const express = require('express');
-// route
+const cors = require('cors');
+const app = express();
 const {userRoutes, orderRoutes, productRoutes} = require('./routes') 
 require('dotenv').config
+const port = process.env.PORT || 6969;
 
-// cors
-const cors = require('cors');
-// port 
-const port = parseInt(process.env.PORT) || 9001;
-// Express app
-const app = express();
-// Middleware
-const {errorHandling} = require('./middleware/ErrorHandling');
-//
-const cookieParser = require('cookie-parser');
+app.set('port', process.env.PORT || 3000);
+app.use(express.json(), cors());
 
+// Allowing frontend access to backend
 app.use((req, res, next)=> {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header("Access-Control-Allow-Credentials", "true")
-        res.header("Access-Control-Allow-Methods", "*")
-        res.header("Access-Control-Allow-Headers", "*")
-        next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next();
 });
 
-app.use(route);
-app.use(
-    cors(),
-    cookieParser(),
-    express.json,
-    express.urlencoded({extended: false})
-)
+// Root Route
+app.get('/', (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, '/view/index.html'));
+})
+// Use router to handle product and user routes
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
-app.use('/orders', orderRoutes)
+app.use('/orders', orderRoutes);
 
-// Server is running
-app.listen(port, ()=> {
-    console.log(`Server is running...`);
-    console.log(`Listening on port ${port}...`);
-});
 
-// Handling all errors
-app.use(errorHandling);
+app.listen(port, () => {
+  console.log('Server is running...');
+  console.log(`Listening on port ${port}...`);
+})
